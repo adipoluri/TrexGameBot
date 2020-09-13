f554b610994021cb7fa945a28feecf0ebb27fb26
adipoluri <adipoluri@gmail.com>
2020-09-13 02:54:10 -0700

    fixed a few bugs

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from PIL import ImageGrab, ImageOps
 import pyautogui
+import keyboard
 import time
 from numpy import *
 
@@ -8,19 +9,24 @@ class Bot:
     dinosaur = (0, 0)
     running = False
 
+
 def pressSpace():
     pyautogui.keyDown("space")
     time.sleep(0.05)
     print("Jump")
     pyautogui.keyUp("space")
 
+
 def imageGrab():
-    box = (Bot.dinosaur[0] + 15, Bot.dinosaur[1] + 15, Bot.dinosaur[0] + 57,
-           Bot.dinosaur[1] + 32)
+    box = (Bot.dinosaur[0] + 15, Bot.dinosaur[1], Bot.dinosaur[0] + 57,
+           Bot.dinosaur[1] + 15)
     image = ImageGrab.grab(box)
     grayImage = ImageOps.grayscale(image)
     a = array(grayImage.getcolors())
     print(a.sum())
+    return a.sum()
+
+screen = imageGrab()
 
 def stopBot():
     Bot.running = False
@@ -30,11 +36,10 @@ def runBot():
         return False
     pressSpace()
     Bot.running = True
-
     print(Bot.dinosaur)
     while Bot.running:
-        screen = str(imageGrab())
-        if screen != 961:
+        if screen != 877:
             pressSpace()
             time.sleep(0.1)
-
+        if keyboard.is_pressed('esc'):
+            break
